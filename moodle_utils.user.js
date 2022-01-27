7f8c16c0ec60c1f869654ca6b84b8f7937d9e82f
Jakub Ostrzołek <01158865@pw.edu.pl>
2022-01-27 01:44:19 +0100

    add update redirect

---
+++ moodle_utils.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name            Moodle Utils
+// @author          Ogurczak
+// @description     Displays time per question left
+// @version         0.8
+// @namespace       https://github.com/Ogurczak/
+// @updateURL       https://github.com/Ogurczak/moodle-utils/raw/main/client/build/moodle_utils.user.js
+// ==/UserScript==
