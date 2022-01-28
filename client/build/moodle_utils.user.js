078ae7c481f54d5e392e9059b1f7335782cef2f3
Jakub Ostrzołek <01158865@pw.edu.pl>
2022-01-28 01:53:02 +0100

    substituted {{pre-push:branch}} with branch name

--- client/build/moodle_utils.user.js
+++ client/build/moodle_utils.user.js
@@ -2,9 +2,9 @@
 // @name            Moodle Utils
 // @author          Ogurczak
 // @description     Displays time per question left
-// @version         0.8
+// @version         0.7.1
 // @namespace       https://github.com/Ogurczak/
-// @updateURL       https://github.com/Ogurczak/moodle-utils/raw/main/client/build/moodle_utils.user.js
+// @updateURL       https://github.com/Ogurczak/moodle-utils/raw/develop/client/build/moodle_utils.user.js
 // @match           https://*/mod/quiz/attempt*
 // @match           https://*/mod/quiz*
 // @match           https://github.com/Ogurczak/moodle-utils*
@@ -14,7 +14,7 @@
 // @grant           GM_setValue
 // @grant           GM_registerMenuCommand
 // @grant           GM_getResourceText
-// @resource        css https://raw.githubusercontent.com/Ogurczak/moodle-utils/main/client/style.css
+// @resource        css https://raw.githubusercontent.com/Ogurczak/moodle-utils/develop/client/style.css
 // @require         https://raw.github.com/odyniec/MonkeyConfig/master/monkeyconfig.js
 // @require         http://code.jquery.com/jquery-3.4.1.min.js
 // ==/UserScript==
