3004edcbed86618c2dee186678342ed64f042223
Jakub Ostrzołek <01158865@pw.edu.pl>
2022-01-28 01:52:52 +0100

    change tsc version

--- client/moodle_utils.user.ts
+++ client/moodle_utils.user.ts
@@ -2,7 +2,7 @@
 // @name            Moodle Utils
 // @author          Ogurczak
 // @description     Displays time per question left
-// @version         0.8
+// @version         0.7.1
 // @namespace       https://github.com/Ogurczak/
 // @updateURL       https://github.com/Ogurczak/moodle-utils/raw/{{pre-push:branch}}/client/build/moodle_utils.user.js
 // @match           https://*/mod/quiz/attempt*
