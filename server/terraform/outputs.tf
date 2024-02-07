484294f23e4971c95c702f042d7317c9ac9f5cb8
krisboorger <krisboorger@gmail.com>
2024-02-07 22:06:09 +0100

    feat(tf): update tf outputs to include tenantId

--- server/terraform/outputs.tf
+++ server/terraform/outputs.tf
@@ -5,3 +5,7 @@ output "resource_group_name" {
 output "kubernetes_cluster_name" {
   value = azurerm_kubernetes_cluster.k8s_cluster.name
 }
+
+output "tenant_id" {
+  value = azurerm_key_vault.key_vault.tenant_id
+}
