d7fd94e6ccbb261148be33d093113d7f56e63381
krisboorger <krisboorger@gmail.com>
2024-02-07 22:07:40 +0100

    feat(deploy): save values for use with k8s

--- server/deploy/deploy_aks.sh
+++ server/deploy/deploy_aks.sh
@@ -5,3 +5,8 @@ set -euo pipefail
 # apply terraform files
 terraform -chdir=terraform init
 terraform -chdir=terraform apply -auto-approve
+
+# save tf values for use with k8s
+cluster_name=$(terraform -chdir=terraform output -raw kubernetes_cluster_name)
+cluster_rg_name=$(terraform -chdir=terraform output -raw resource_group_name)
+tenant_id=$(terraform -chdir=terraform output -raw tenant_id)
