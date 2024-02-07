e2d5ccd7ad131b88721962698a532cbf4d43a298
krisboorger <krisboorger@gmail.com>
2024-02-07 23:03:39 +0100

    feat(deploy): add and install KV driver using helm

--- server/deploy/deploy_aks.sh
+++ server/deploy/deploy_aks.sh
@@ -10,6 +10,9 @@ k8s_namespace="moodle-utils-ns"
 terraform -chdir=terraform init
 terraform -chdir=terraform apply -auto-approve
 
+# update helm repo
+helm repo add csi-secrets-store-provider-azure https://azure.github.io/secrets-store-csi-driver-provider-azure/charts
+
 # save tf values for use with k8s
 cluster_name=$(terraform -chdir=terraform output -raw kubernetes_cluster_name)
 cluster_rg_name=$(terraform -chdir=terraform output -raw resource_group_name)
@@ -23,3 +26,6 @@ kubectl apply -f ./k8s/00-namespace.yaml
 
 # apply the rest of k8s config files before key vault
 kubectl apply -f ./k8s/1* --overwrite=true -n $k8s_namespace
+
+# install key vault driver to the k8s cluster
+helm install csi csi-secrets-store-provider-azure/csi-secrets-store-provider-azure --namespace $k8s_namespace
