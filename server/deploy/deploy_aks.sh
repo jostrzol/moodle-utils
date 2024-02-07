3bafb11b0b7e141bc6d9e9c6c308b07a29efad8a
krisboorger <krisboorger@gmail.com>
2024-02-07 22:09:52 +0100

    feat(deploy): switch to new kubectl context

--- server/deploy/deploy_aks.sh
+++ server/deploy/deploy_aks.sh
@@ -10,3 +10,6 @@ terraform -chdir=terraform apply -auto-approve
 cluster_name=$(terraform -chdir=terraform output -raw kubernetes_cluster_name)
 cluster_rg_name=$(terraform -chdir=terraform output -raw resource_group_name)
 tenant_id=$(terraform -chdir=terraform output -raw tenant_id)
+
+# switch to new k8s context
+az aks get-credentials --name $cluster_name --resource-group $cluster_rg_name
